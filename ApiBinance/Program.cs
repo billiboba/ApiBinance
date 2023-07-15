3f9c2a76217293d6740705eb668178bd31a84820
Roma.Kor <romakor.19@yandex.ru>
2023-07-16 03:32:19 +0700

    Добавьте файлы проекта.

---
+++ ApiBinance/Program.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+namespace BinanceFuturesAccount
+
+{
+    class Program
+    {
+        public const string apiKey = "Your_Api";
+        public const string secretKey = "Your_key";
+        public const string baseUrl = "https://fapi.binance.com";
+
+
+        public class FuturesAccountInfo
+        {
+            [JsonProperty("assets")]
+            public List<FuturesAssetBalance> Balances { get; init; }
+        }
+
+        public class FuturesAssetBalance
+        {
+            [JsonProperty("asset")]
+            public string Asset { get; init; }
+
+            [JsonProperty("availableBalance")]
+            public double AvailableBalance { get; init; }
+        }
+        private static string CalculateSignature(string secretKey, string payload)
+        {
+            var encoding = new UTF8Encoding();
+            byte[] keyBytes = encoding.GetBytes(secretKey);
+            byte[] payloadBytes = encoding.GetBytes(payload);
+
+            using (var hmacSha256 = new HMACSHA256(keyBytes))
+            {
+                byte[] signatureBytes = hmacSha256.ComputeHash(payloadBytes);
+                return BitConverter.ToString(signatureBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        public static async Task<double> GetAccountInfo()
+        {
+            long timestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+            string queryString = $"timestamp={timestamp}";
+            string signature = CalculateSignature(secretKey, queryString);
+            string endpoint = "/fapi/v2/account";
+            string url = $"{baseUrl}{endpoint}?{queryString}&signature={signature}";
+            string asset = "USDT";
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-MBX-APIKEY", apiKey);
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var accountInfo = JsonConvert.DeserializeObject<FuturesAccountInfo>(jsonResponse);
+                var balance = accountInfo.Balances.FirstOrDefault(b => b.Asset == asset);
+                Console.WriteLine(response.StatusCode);
+                //Console.WriteLine(responseBody);
+                return balance.AvailableBalance;
+            }
+            else
+            {
+                throw new Exception("Ошибка запроса");
+            }
+        }
+        static string CreateQueryString(Dictionary<string, string> parameters)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var parameter in parameters)
+            {
+                stringBuilder.Append($"{parameter.Key}={parameter.Value}&");
+            }
+            var queryString = stringBuilder.ToString().TrimEnd('&');
+            return queryString;
+        }
+        public static async Task BuySell(string symbol, string side, string type, string quantity)
+        {
+            long timestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+            string endpointPath = "/fapi/v1/order";
+            var parameters = new Dictionary<string, string>
+        {
+            { "symbol", symbol },  // Symbol of the futures contract
+            { "side", side },        // SIDE: BUY or SELL
+            { "type", type },     // TYPE: MARKET, LIMIT, STOP_MARKET etc.
+            { "quantity", quantity },  // Quantity to buy or sell
+            { "timestamp", timestamp.ToString() },  // Current timestamp
+            };
+            var payload = CreateQueryString(parameters);
+            var signature = CalculateSignature(secretKey, payload);
+
+            // Добавление подписи и ключа API в заголовок запроса
+            var requestUri = $"{baseUrl}{endpointPath}?{payload}&signature={signature}";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-MBX-APIKEY", apiKey);
+
+            // Отправка POST запроса
+            var response = await client.PostAsync(requestUri, null);
+
+            // Получение ответа
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+
+        public static async Task Main()
+        {
+            var balance = await GetAccountInfo();
+            Console.WriteLine(balance);
+            string symbol = "SKLUSDT";
+            string side = "SELL";
+            string type = "MARKET";
+            string quantity = "180";
+            await BuySell(symbol, side, type, quantity);
+        }
+    }
+}

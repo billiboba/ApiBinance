5a803396162b45e71c28da59323812124f24711e
Roma.Kor <romakor.19@yandex.ru>
2023-09-04 22:44:12 +0700

    Add method GetClosedPosition

--- ApiBinance/Main.cs
+++ ApiBinance/Main.cs
@@ -8,15 +8,18 @@ namespace BinanceFuturesAccount
     {
         public static async Task Main()
         {
-            
+            var aps = await WebBinance.GetAccountBalance();
+            Console.WriteLine(aps);
+            await WebBinance.GetClosedPosition();
             while (true)
             {
-                Dictionary<string, double> open = await WebBinance.GetOpenPositionFutures();
-                foreach(var pos in open)
-                {
-                    Console.WriteLine(pos.Key + pos.Value);
-                }
-                Thread.Sleep(1000);
+                //Dictionary<string, double> open = await WebBinance.GetOpenPositionFutures();
+                //foreach(var pos in open)
+                //{
+                //    Console.WriteLine(pos.Key + pos.Value);
+                //}
+                //Thread.Sleep(1000);
+                
             }
 
         }
--- ApiBinance/WebBinance.cs
+++ ApiBinance/WebBinance.cs
@@ -42,33 +42,16 @@ namespace ApiBinance
 
             [JsonProperty("totalMarginBalance")]
             public double totalMarginBalance { get; init; }
-        }
-        public class Profit
-        {
-            [JsonProperty("unRealizedProfit")]
-            public double unRealizedProfit { get; init; }
+
+            [JsonProperty("realizedPnl")]
+            public double realizedPnl { get;init; }
         }
 
-        //public static async Task GetOpenPositionFutures() //Информация о сделке(Профит по сделке)
-        //{
-        //    long timestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
-        //    string queryString = $"timestamp={timestamp}";
-        //    string signature = BaseInfo.CalculateSignature(BaseInfo.secretKey, queryString);
-        //    string endpoint = "/fapi/v2/positionRisk";
-        //    string url = $"{BaseInfo.baseUrl}{endpoint}?{queryString}&signature={signature}";
-        //    HttpClient client = new HttpClient();
-        //    client.DefaultRequestHeaders.Add("X-MBX-APIKEY", BaseInfo.apiKey);
-        //    HttpResponseMessage response = await client.GetAsync(url);
-        //    string responseBody = await response.Content.ReadAsStringAsync();
-        //    List<FuturesAssetBalance> positions = JsonConvert.DeserializeObject<List<FuturesAssetBalance>>(responseBody);
-        //    foreach (FuturesAssetBalance position in positions)
-        //    {
-        //        if (position.EntryPrice != 0)
-        //        {
-        //            Console.WriteLine($"Символ: {position.symbol}, Profit: {position.unRealizedProfit}");
-        //        }
-        //    }
-        //}
+        public class PositionClosed
+        {
+
+        }
+
         public static async Task<Dictionary<string,double>> GetOpenPositionFutures() //Информация о сделке(Профит по сделке)//Переделать, скорее всего нужно, чтобы данные выводились в массив.
         {
             long timestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
@@ -107,17 +90,44 @@ namespace ApiBinance
             string url = $"{BaseInfo.baseUrl}{endpoint}?{queryString}&signature={signature}";
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("X-MBX-APIKEY", BaseInfo.apiKey);
-
+            Console.WriteLine(url);
             HttpResponseMessage response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 var jsonResponse = await response.Content.ReadAsStringAsync();
                 var accountInfo = JsonConvert.DeserializeObject<FuturesAssetBalance>(jsonResponse);
+
                 return accountInfo.totalMarginBalance;
             }
             else
             {
                 throw new Exception("Ошибка запроса");
+            }
+        }
+       
+        
+        public static async Task GetClosedPosition()
+        {
+            long timestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds;
+            string baseUrl = "https://fapi.binance.com";
+            //string endpoint = "/fapi/v1/trades";
+            //string queryString = $"symbol=ZENUSDT&timestamp={GetTimestamp()}";
+            string endpoint = "/fapi/v1/userTrades";
+            string queryString = $"&timestamp={timestamp}";
+            string signature = BaseInfo.CalculateSignature(BaseInfo.secretKey, queryString);
+            string url = $"{baseUrl}{endpoint}?{queryString}&signature={signature}";   
+            Console.WriteLine(url);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-MBX-APIKEY", BaseInfo.apiKey);
+
+            HttpResponseMessage response = await client.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+            List<FuturesAssetBalance> positions = JsonConvert.DeserializeObject<List<FuturesAssetBalance>>(responseBody);
+            var test = new Dictionary<string, double>();
+            foreach (FuturesAssetBalance position in positions)
+            {
+                Console.WriteLine(position.symbol + position.realizedPnl);
             }
         }
     }
